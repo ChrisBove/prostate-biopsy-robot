aa8e8e184958e5fd43c785868246c0472ff79677
ngoldfarb <nagoldfarb@wpi.edu>
2017-04-17 18:37:04 -0400

    new dynamic_model

--- pbb_model/src/dynamic_model.py
+++ pbb_model/src/dynamic_model.py
@@ -8,22 +8,54 @@ from std_msgs.msg import String
 
 class DynamicModel(object):
     """docstring fo dynamic_model."""
-    def __init__(self, M, B, K):
+    def __init__(self):
 
-        self._M = M
-        self._B = B
-        self._K = K
-        (self._P, self._Q ) = self.get_needle_forces()
+        self._l = 10
+        self._Cl = [ ]
+        self._P = 2/l*np.array([1,1,1,1,1,1])
+        #(self._P, self._Q ) = self.get_needle_forces()
+        self._N = 25
+        self._G = 10^6 #shear modulus
+        self._J = 100 #polar moment Interia
+        self._pho = 10
 
-    def inverseModel(self, ):
-        return
+
+    def get_d_coef(self,l,v,beta):
+        """This function get the little d coef """
+        d = [][]
+
+        for i in xrange(1,self._N):
+            for j in xrange(1,self._N):
+
+                if (j > i):
+                    d[i-1][j-1] = ( -4* ((-1)**(i-j))*(j-1)**2)/ ( ( j-i )*( i + j - 2))
+                elif ( i == j ):
+                    d[i-1][j-1] = np.sign( 1 - i) + (beta*l)/(self._J*self._pho*v)
+                elif ( j < i):
+                    d[i-1][j-1] = ( 4* ((-1)**(j-i))*(j-1)**2)/ ((i -j)* ( i + j - 2))
+        return d
 
 
-    """
-        Calulates the force acting on the needle tip
-        returns P and Q
-    """
+
+    def get_k_coef(self, ):
+        k = [][]
+        for i in xrange(1,self._N):
+            for j in xrange(1,self._N):
+                if (i == 0 or j ==0):
+                    k[i-1][j-1] = 0
+                elif ( i == j ):
+                    k[i-1][j-1] = np.sign( 1 - i) + (beta*l)/(self._J*self._pho*v)
+                elif ( j < i):
+                    k[i-1][j-1] = ( 4* ((-1)**(j-i))*(j-1)**2)/ ((i -j)* ( i + j - 2))
+
+
+
+
     def get_needle_forces(self):
+        """
+            Calulates the force acting on the needle tip
+            returns P and Q
+        """
 
         lamada = np.pi/6 # bevel angle
         a = 1 # tip lenght
@@ -34,18 +66,13 @@ class DynamicModel(object):
         T = 0.5*Kt*b*b
 
         alpha = ( m.tan(lamada) - m.tan(beta)) / ( 1 + m.tan(lamada)*m.tan(beta))
-        # force acting paralle with the driving force of the needle
-        P = T *m.sin(lamada*alpha )
 
+        # see diagram for force
+        P = T *m.sin(lamada*alpha )
         Q = T*m.cos(lamada*alpha) - 0.5*Kt*a*a*m.tan(beta)
 
         return (P, Q)
 
-
-
-
-
-        pass
 
 if  __name__ == '__main__':
     rospy.init_node('dynamic_model', anonymous=True)
