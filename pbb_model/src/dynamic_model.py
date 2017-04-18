810bd05291ba7d3279325eee1a2106e9a0df50ce
ngoldfarb <nagoldfarb@wpi.edu>
2017-04-18 19:31:33 -0400

    added test file for dynamics

--- pbb_model/src/dynamic_model.py
+++ pbb_model/src/dynamic_model.py
@@ -16,12 +16,12 @@ class DynamicModel(object):
 
         self._l = 10
         self._depth = 0
-        self._Cl = []
-        self._P =  np.asmatrix( 2 / l * np.ones((self._N, 1)))
-        self._C0 = np.asmatrix(  np.ones((1, self._N)))
-        np.put(self._C0,[0],[0.5])
+        self._N = 5
+        self._P = np.asmatrix(2 / self._l * np.ones((self._N, 1)))
+        self._C0 = np.asmatrix(np.ones((1, self._N)))
+        np.put(self._C0, [0], [0.5])
         #(self._P, self._Q ) = self.get_needle_forces()
-        self._N = 25
+
         self._G = 1.841210  # shear mo(dulus
         self._J = 1.5962 * 10**(-14)  # polar momemoment Interia
         self._pho = 6.453
@@ -29,71 +29,69 @@ class DynamicModel(object):
         self._beta = 4.753 * 10**(-3)
         self._kappa = 13.698630137  # kappa
 
-        self._Cl =  np.ones((1, self._N))
-        for ii in xrange(25):
-	        np.put(self._Cl, [ii], [ (-1)**ii ] )
-        np.put(self._Cl, [0], [ .5 ] )
+        self._Cl = np.ones((1, self._N))
+        for ii in xrange(self._N):
+            np.put(self._Cl, [ii], [(-1)**ii])
+        np.put(self._Cl, [0], [.5])
 
-        self._s_state = []
-        self._q_state = np.matrix
+        self._s_state = np.matrix([[0], [0], [0], [0], [0], [0]])
+        self._q_state = np.asmatrix(np.zeros((self._N)))
 
-
-
-    def update(self, u1, u2,dt):
+    def update(self, u1, u2, dt):
         """Updates the systems"""
-        self.update_depth()
+        print "hello"
+        self.update_depth(u1, dt)
+
         A = self.get_Amat(u1)
         B = self.get_Bmat(u1)
         C = self.get_Cmat(A)
         D = self.get_Dmat(A)
-        (n, self._s_state) = self.update_S(u1,u2,C,D,T)
-        self._q_state = A*self._q_state + B*u
-        return (self._s_state, self._q_state  )
+        (n, self._s_state) = self.update_S(u1, u2, C, D, T)
+        self._q_state = A * self._q_state + B * u
+        return (self._s_state, self._q_state)
 
-
-    def update_depth(self, v,dt):
+    def update_depth(self, v, dt):
         """This updates the insertion depth"""
-        self._depth = self._depth + v*dt
+        self._depth = self._depth + v * dt
 
-    def update_S(self, u1, u2, C, D,T):
+    def update_S(self, u1, u2, C, D, T):
         """Update the S state"""
 
         # this are the unit vectors to move it into the right frame
-        e1 = np.array([[1],[0],[0]])
-        e3 = np.array([[0],[0],[1]])
-        V1 = np.concatenate(([[e3],[self._k*e1]]), axis=0)
-        V2 = np.concatenate(([[0],[0],[0]],e3), axis=0)
-        V1_hat = np.concatenate((isomorphic(V1[1]),V1[0]), axis=1)
-        V1_hat = np.vstack((V1_hat,np.array([0,0,0,0])))
-        V2_hat = np.concatenate((isomorphic(V2[3:6]),V2[0:3]), axis=1)
-        V2_hat = np.vstack((V2_hat,np.array([0,0,0,0])))
+        e1 = np.array([[1], [0], [0]])
+        e3 = np.array([[0], [0], [1]])
+        V1 = np.concatenate(([[e3], [self._k * e1]]), axis=0)
+        V2 = np.concatenate(([[0], [0], [0]], e3), axis=0)
+        V1_hat = np.concatenate((isomorphic(V1[1]), V1[0]), axis=1)
+        V1_hat = np.vstack((V1_hat, np.array([0, 0, 0, 0])))
+        V2_hat = np.concatenate((isomorphic(V2[3:6]), V2[0:3]), axis=1)
+        V2_hat = np.vstack((V2_hat, np.array([0, 0, 0, 0])))
         # this is where the state is updated
-        J = self.get_jacobian(self._s_state[4],self._s_state[5])
-        V = J*(V1_hat*u1 + V2_hat*( C*self._q_state + D*u2) )*T
-        new_state = p.array([self._s_state[:,:].dot(expm(V))])
-        n = ((new_state[0,0:3,0:3]*l2).dot(e3)+(new_state[0,0:3,3]).reshape(3,1))
+        J = self.get_jacobian(self._s_state[4], self._s_state[5])
+        V = J * (V1_hat * u1 + V2_hat * (C * self._q_state + D * u2)) * T
+        new_state = p.array([self._s_state[:, :].dot(expm(V))])
+        n = ((new_state[0, 0:3, 0:3] * l2).dot(e3) +
+             (new_state[0, 0:3, 3]).reshape(3, 1))
 
         return (n, new_state)
 
-
     def isomorphic(x):
-        return np.array([[0,-x.item(2),x.item(1)],
-    		[x.item(2),0,-x.item(0)],
-    		[-x.item(1),x.item(0),0]])
-
+        return np.array([[0, -x.item(2), x.item(1)],
+                         [x.item(2), 0, -x.item(0)],
+                         [-x.item(1), x.item(0), 0]])
 
     def get_Amat(self, v):
         """This function calculate the A matrix"""
 
         invD = self.get_invD(v)
-        K = self.get_K( v)
+        K = self.get_K(v)
         # this is link a sring constant
         lumped_sping = (self._J * self._G) / (self._l - self._depth)
         makeMatrix = K + lumped_sping * self._P * self._C0
-        A = -invD * makeMatrix
+        A = invD * makeMatrix
         return A
 
-    def get_Bmat(self,l,v):
+    def get_Bmat(self, l, v):
         """This function calculate the B matrix"""
         invD = self.get_invD(v)
         # this is link a sring constant
@@ -103,28 +101,30 @@ class DynamicModel(object):
 
     def get_Cmat(self, A):
         """This function calculate the C matrix"""
-        return self._Cl*A
+        return self._Cl * A
 
     def get_Dmat(self, B):
         """This function returns the D matrix"""
-        return self._Cl*B
+        return self._Cl * B
 
     def get_invD(self,  v):
         """get the D matrix"""
 
-        d = self.get_d_coef(l, v):
-        D = numpy.zeros((self._N, self._N))
+        d = self.get_d_coef(v)
+
+        D = np.zeros((self._N, self._N))
         coef = (self._J * self._pho * v) / self._depth
 
-        for i in xrange(1, self_N):
-            for j in xrange(1, self_N):
+        for i in xrange(self._N):
+            for j in xrange(self._N):
                 D[i][j] = coef * d[i][j]
-        return np.linalg.inv(np.asmatrix(D))
+        print D
+        #return np.linalg.inv(np.asmatrix(D))
 
-    def get_d_coef(self, l, v):
+    def get_d_coef(self, v):
         """This function get the little d coef """
 
-        d = [][]
+        d = [[0 for i in xrange(self._N)] for i in xrange(self._N)]
         for i in xrange(1, self._N):
             for j in xrange(1, self._N):
 
@@ -133,27 +133,28 @@ class DynamicModel(object):
                                        * (j - 1)**2) / ((j - i) * (i + j - 2))
                 elif (i == j):
                     d[i - 1][j - 1] = np.sign(1 - i) + \
-                        (self._beta * l) / (self._J * self._pho * v)
+                        (self._beta * self._depth) / (self._J * self._pho * v)
                 elif (j < i):
                     d[i - 1][j - 1] = (4 * ((-1)**(j - i))
                                        * (j - 1)**2) / ((i - j) * (i + j - 2))
+
         return d
 
     def get_K(self, v):
         """get the K matrix"""
 
-        d = self.get_k_coef(l):
-        K = numpy.zeros((self._N, self._N))
+        k = self.get_k_coef(v)
+        K = np.zeros((self._N, self._N))
         coef = (self._J * self._pho * v * v) / (self._depth**2)
 
-        for i in xrange(1, self_N):
-            for j in xrange(1, self_N):
-                K[i][j] = coef * d[i][j]
-        return np.asmatrix( K )
+        for i in xrange(self._N):
+            for j in xrange(self._N):
+                K[i][j] = coef * k[i][j]
+        return np.asmatrix(K)
 
-    def get_k_coef(self):
+    def get_k_coef(self,v):
         """This function get the little k coef """
-        k = [][]
+        k = [[0 for i in xrange(self._N)] for i in xrange(self._N)]
         for i in xrange(1, self._N):
             for j in xrange(1, self._N):
                 if (i == 0 or j == 0):
@@ -166,14 +167,12 @@ class DynamicModel(object):
                                        (-1)**(i - j)) / ((j - i)**2 * (i + j - 2)**2)
         return k
 
-
     def get_jacobian(self, beta, gamma):
         """calculates the Jacobian"""
-        J = np.matrix( [ [ m.cos(beta)*m.cos(gamma), m.sin(gamma), 0 ], \
-                         [ -m.cos(beta)*m.sin(gamma), m.cos(gamma), 0], \
-                         [ m.sin(beta), 0, 1 ]])
+        J = np.matrix([[m.cos(beta) * m.cos(gamma), m.sin(gamma), 0],
+                       [-m.cos(beta) * m.sin(gamma), m.cos(gamma), 0],
+                       [m.sin(beta), 0, 1]])
         return J
-
 
     def get_needle_forces(self):
         """
---
+++ pbb_model/src/dynamics_testing.py
@@ -0,0 +1,5 @@
+
+import dynamic_model
+
+needle = dynamic_model.DynamicModel()
+print needle.update(2,2,.1)
