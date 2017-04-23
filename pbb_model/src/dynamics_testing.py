a7f3fd2c3f386c96fa6b247e1da92ac230735ddd
ngoldfarb <nagoldfarb@wpi.edu>
2017-04-23 19:41:31 -0400

    fixed the ploting

--- pbb_model/src/dynamics_testing.py
+++ pbb_model/src/dynamics_testing.py
@@ -2,18 +2,16 @@
 import dynamic_model
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 from mpl_toolkits.mplot3d import Axes3D
 needle = dynamic_model.DynamicModel()
 
-n = needle.update( 0.001 , 0 ,1)
-for i in xrange(1000):
-
-    n =  np.hstack( (n,needle.update(.001 , 0,1)))
+n = needle.update(.05,.05,.1)
+for i in xrange(300):
+    n =  np.hstack( (n,needle.update(.05,.05,.1)))
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(n[0,1:], n[1,1:], n[2,1:])
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(n[0,1:], n[1,1:], n[2,1:], label='parametric curve')
 
 ax.legend()
 
