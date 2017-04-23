f254271c5177a54dd5dbdd09a2317e2921c82b20
Chris Bove <bove.3123@gmail.com>
2017-04-23 15:33:39 -0400

    correct indentation

--- pbb_visualization/src/marker_node.cpp
+++ pbb_visualization/src/marker_node.cpp
@@ -40,84 +40,84 @@ using namespace visualization_msgs;
 ros::Publisher pointPub;
 
 void processFeedback(
-    const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
+		const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
 {
-  ROS_INFO_STREAM( feedback->marker_name << " is now at "
-      << feedback->pose.position.x << ", " << feedback->pose.position.y
-      << ", " << feedback->pose.position.z );
+	ROS_INFO_STREAM( feedback->marker_name << " is now at "
+			<< feedback->pose.position.x << ", " << feedback->pose.position.y
+			<< ", " << feedback->pose.position.z );
 
-  geometry_msgs::PointStamped msg;
-  msg.header.frame_id = "gelatin";
-  msg.header.stamp = ros::Time::now();
-  msg.point = feedback->pose.position;
-  pointPub.publish(msg);
+	geometry_msgs::PointStamped msg;
+	msg.header.frame_id = "gelatin";
+	msg.header.stamp = ros::Time::now();
+	msg.point = feedback->pose.position;
+	pointPub.publish(msg);
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "marker_node");
-  ros::NodeHandle nh;
+	ros::init(argc, argv, "marker_node");
+	ros::NodeHandle nh;
 
-  pointPub = nh.advertise<geometry_msgs::PointStamped>( "start_point", 1 );
+	pointPub = nh.advertise<geometry_msgs::PointStamped>( "start_point", 1 );
 
-  // create an interactive marker server on the topic namespace simple_marker
-  interactive_markers::InteractiveMarkerServer server("marker_node");
+	// create an interactive marker server on the topic namespace simple_marker
+	interactive_markers::InteractiveMarkerServer server("marker_node");
 
-  // create an interactive marker for our server
-  visualization_msgs::InteractiveMarker int_marker;
-  int_marker.header.frame_id = "gelatin";
-  int_marker.header.stamp=ros::Time::now();
-  int_marker.name = "start_marker";
-  int_marker.description = "Start Marker";
-  int_marker.scale = 0.05;
+	// create an interactive marker for our server
+	visualization_msgs::InteractiveMarker int_marker;
+	int_marker.header.frame_id = "gelatin";
+	int_marker.header.stamp=ros::Time::now();
+	int_marker.name = "start_marker";
+	int_marker.description = "Start Marker";
+	int_marker.scale = 0.05;
 
-  // create a grey box marker
-  visualization_msgs::Marker box_marker;
-  box_marker.type = visualization_msgs::Marker::SPHERE;
-  box_marker.scale.x = 0.01;
-  box_marker.scale.y = 0.01;
-  box_marker.scale.z = 0.01;
-  box_marker.color.r = 0.0;
-  box_marker.color.g = 1.0;
-  box_marker.color.b = 0.0;
-  box_marker.color.a = 1.0;
+	// create a grey box marker
+	visualization_msgs::Marker box_marker;
+	box_marker.type = visualization_msgs::Marker::SPHERE;
+	box_marker.scale.x = 0.01;
+	box_marker.scale.y = 0.01;
+	box_marker.scale.z = 0.01;
+	box_marker.color.r = 0.0;
+	box_marker.color.g = 1.0;
+	box_marker.color.b = 0.0;
+	box_marker.color.a = 1.0;
 
-  // create a non-interactive control which contains the box
-  visualization_msgs::InteractiveMarkerControl box_control;
-  box_control.always_visible = true;
-  box_control.markers.push_back( box_marker );
+	// create a non-interactive control which contains the box
+	visualization_msgs::InteractiveMarkerControl box_control;
+	box_control.always_visible = true;
+	box_control.markers.push_back( box_marker );
 
-  // add the control to the interactive marker
-  int_marker.controls.push_back( box_control );
+	// add the control to the interactive marker
+	int_marker.controls.push_back( box_control );
 
-  visualization_msgs::InteractiveMarkerControl control;
+	visualization_msgs::InteractiveMarkerControl control;
 
-  control.orientation.w = 1;
-  control.orientation.x = 0;
-  control.orientation.y = 1;
-  control.orientation.z = 0;
-  control.name = "move_z";
-  control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-  int_marker.controls.push_back(control);
+	control.orientation.w = 1;
+	control.orientation.x = 0;
+	control.orientation.y = 1;
+	control.orientation.z = 0;
+	control.name = "move_z";
+	control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
+	int_marker.controls.push_back(control);
 
-  control.orientation.w = 1;
-  control.orientation.x = 0;
-  control.orientation.y = 0;
-  control.orientation.z = 1;
-  control.name = "move_y";
-  control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
-  int_marker.controls.push_back(control);
+	control.orientation.w = 1;
+	control.orientation.x = 0;
+	control.orientation.y = 0;
+	control.orientation.z = 1;
+	control.name = "move_y";
+	control.interaction_mode = InteractiveMarkerControl::MOVE_AXIS;
+	int_marker.controls.push_back(control);
 
-  // add the control to the interactive marker
-  int_marker.controls.push_back(control);
+	// add the control to the interactive marker
+	int_marker.controls.push_back(control);
 
-  // add the interactive marker to our collection &
-  // tell the server to call processFeedback() when feedback arrives for it
-  server.insert(int_marker, &processFeedback);
+	// add the interactive marker to our collection &
+	// tell the server to call processFeedback() when feedback arrives for it
+	server.insert(int_marker, &processFeedback);
 
-  // 'commit' changes and send to all clients
-  server.applyChanges();
+	// 'commit' changes and send to all clients
+	server.applyChanges();
 
-  // start the ROS main loop
-  ros::spin();
+	// start the ROS main loop
+	ros::spin();
 }
